b9b5533c73a383f39495402e3a60f6d2ed8f54af
Lucas Henderson <henderson.lucas.j@gmail.com>
2020-02-23 10:55:46 -0500

    Fix merge issues with V30

--- src/DotNeet.Core/CPU/V30.fs
+++ src/DotNeet.Core/CPU/V30.fs
@@ -1,5 +1,5 @@
-<<<<<<< HEAD:v30.fs
 (* NEC V30 CPU implementation *)
+module V30
 
 (* Wrapper type around a V30 general-purpose register to avoid out-of-bounds access *)
 type Register =
@@ -8,22 +8,6 @@ type Register =
 (* Create a Register from an integer *)
 let registerOfInt idx =
     if idx >= 0 && idx <= 7 then Some({ reg = idx }) else None
-=======
-﻿module V30
-(* NEC V30 CPU implementation *)
-
-(* Wrapper type around a V30 general-purpose register to avoid out-of-bounds access *)
-type Register = {
-    reg: int
-}
-
-(* Create a Register from an integer *)
-let registerOfInt idx =
-    if idx >= 0 && idx <= 7 then
-        Some({ reg = idx })
-    else
-        None
->>>>>>> Create solution structure and unit test project:src/DotNeet.Core/CPU/V30.fs
 
 (* Return the encapsulated integer of a Register *)
 let intOfRegister reg = reg.reg
@@ -48,25 +32,12 @@ let SpRegister = { reg = 7 }
 
 
 (* Wrapper type around a V30 segment register to avoid out-of-bounds access *)
-<<<<<<< HEAD:v30.fs
 type Segment =
     { seg: int }
 
 (* Create a Segment from an integer *)
 let segmentOfInt idx =
     if idx >= 0 && idx <= 3 then Some({ seg = idx }) else None
-=======
-type Segment = {
-    seg: int
-}
-
-(* Create a Segment from an integer *)
-let segmentOfInt idx =
-    if idx >= 0 && idx <= 3 then
-        Some({seg = idx})
-    else
-        None
->>>>>>> Create solution structure and unit test project:src/DotNeet.Core/CPU/V30.fs
 
 (* Return the encapsulated integer of a Segment *)
 let intOfSegment seg = seg.seg
@@ -86,7 +57,6 @@ let SsSegment = { seg = 3 }
 [<System.FlagsAttribute>]
 type Flags =
     (* The last operation resulted in unsigned overflow *)
-<<<<<<< HEAD:v30.fs
     | Carry = 1us
     (* The least significant byte of the last operation has an even number of bits *)
     | Parity = 4us
@@ -116,38 +86,6 @@ type CPURegisters =
       seg: uint16 array
       (* The CPU status word *)
       flags: Flags }
-=======
-    | Carry     = 0x0001us
-    (* The least significant byte of the last operation has an even number of bits *)
-    | Parity    = 0x0004us
-    (* The last operation produced a carry from bit 3 to bit 4 *)
-    | Adjust    = 0x0010us
-    (* The last operation produced a zero result *)
-    | Zero      = 0x0040us
-    (* The last operation produced a negative result *)
-    | Sign      = 0x0080us
-    (* The CPU is in single-step mode and will produce an interrupt after executing the next instruction *)
-    | Trap      = 0x0100us
-    (* The CPU will respond to external interrupts *)
-    | Interrupt = 0x0200us
-    (* The CPU will execute string operations from high addresses to low addresses instead of low to high *)
-    | Direction = 0x0400us
-    (* The last operation resulted in signed overflow *)
-    | Overflow  = 0x0800us
-
-
-(* Wrapper type around a register array that only accepts Registers *)
-type CPURegisters = {
-    (* The general-purpose register file *)
-    reg: uint16 array;
-    (* The current instruction pointer *)
-    ip: uint16;
-    (* The current memory segments *)
-    seg: uint16 array;
-    (* The CPU status word *)
-    flags: Flags;
-}
->>>>>>> Create solution structure and unit test project:src/DotNeet.Core/CPU/V30.fs
 
 (* Return a general-purpose register *)
 let getRegister cpu gpr = cpu.reg.[intOfRegister gpr]
@@ -159,8 +97,4 @@ let getInstructionPointer cpu = cpu.ip
 let getSegment cpu seg = cpu.seg.[intOfSegment seg]
 
 (* Return the CPU status word *)
-<<<<<<< HEAD:v30.fs
 let getFlags cpu = cpu.flags
-=======
-let getFlags cpu = cpu.flags
->>>>>>> Create solution structure and unit test project:src/DotNeet.Core/CPU/V30.fs
