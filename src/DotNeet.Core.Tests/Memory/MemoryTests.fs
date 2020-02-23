350193928830a0706b87e628d184839b109c7aab
Lucas Henderson <henderson.lucas.j@gmail.com>
2020-02-23 13:39:07 -0500

    Add Memory module with basic read/write functions

---
+++ src/DotNeet.Core.Tests/Memory/MemoryTests.fs
@@ -0,0 +1,38 @@
+﻿namespace DotNeet.Core.Tests.Memory
+
+open NUnit.Framework
+
+[<TestFixture>]
+type MemoryTests () =
+
+    [<SetUp>]
+    member this.SetUp() =
+        Memory.zero
+
+    [<Test>]
+    member this.CanWriteSingleBytes() =
+        Memory.writeByte 0 0xAAuy
+        Memory.writeByte 1 0xBBuy
+        Memory.writeByte 2 0xCCuy
+        Memory.writeByte 3 0xDDuy
+
+        let byteValue = Memory.readByte 0
+        let shortValue = Memory.readShort 0
+        let intValue = Memory.readInt 0
+
+        Assert.That(byteValue, Is.EqualTo(0xAAu))
+        Assert.That(shortValue, Is.EqualTo(0xBBAAu))
+        Assert.That(intValue, Is.EqualTo(0xDDCC_BBAAu))
+
+    [<Test>]
+    member this.CanWriteMultipleBytes() =
+        let data = [| 0x11uy; 0x22uy; 0x33uy; 0x44uy |]
+        Memory.writeBytes 0 data
+
+        let byteValue = Memory.readByte 0
+        let shortValue = Memory.readShort 0
+        let intValue = Memory.readInt 0
+
+        Assert.That(byteValue, Is.EqualTo(0x11u))
+        Assert.That(shortValue, Is.EqualTo(0x2211u))
+        Assert.That(intValue, Is.EqualTo(0x4433_2211u))
---
+++ src/DotNeet.Core/Memory/Memory.fs
@@ -0,0 +1,18 @@
+﻿(* 384kb 32-bit RAM *)
+module Memory
+
+let busWidth = 16
+let ramSize = 384 * 1024 * busWidth / 8
+let wordSize = busWidth / 8
+
+let data = Array.create ramSize 0uy
+let zero = System.Array.Fill(data, 0uy)
+
+let readShift addr bits = uint32 data.[addr] <<< bits
+
+let readByte addr = data.[addr]
+let readShort addr = readShift (addr + 1) 8 ||| uint32 (readByte addr)
+let readInt addr = readShort (addr + 2) <<< 16 ||| readShort addr
+
+let writeByte addr (value: byte) = data.[addr] <- value
+let writeBytes addr (values: byte[]) = System.Array.Copy(values, 0, data, addr, values.Length)
